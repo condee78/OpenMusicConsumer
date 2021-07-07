48809dea3a53e160f19b53b25c8d61d7209ae626
condee78 <condee78@gmail.com>
2021-07-07 11:22:34 +0700

    export playlists based on playlistsId

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,15 +5,15 @@ class PlaylistsService {
     this._pool = new Pool();
   }
 
-  async getPlaylists(userId) {
+  async getPlaylists(playlistId) {
     const query = {
       text: `SELECT playlists.id, playlists.name, users.username
       FROM playlists
       LEFT JOIN users ON users.id = playlists.owner
       LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
-      WHERE playlists.owner = $1 OR collaborations.user_id = $1
+      WHERE playlists.id = $1
       GROUP BY playlists.id, users.username`,
-      values: [userId],
+      values: [playlistId],
     };
     const result = await this._pool.query(query);
     return result.rows;
